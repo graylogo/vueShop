1d164250287f8ad81bd1932e3d5fe517d1fc09b2
graylogo <graylogo@gmail.com>
2021-04-24 14:49:08 +0800

    debug

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+// vue.config.vue用于自定义配置
+// 自定义启动端口
+
+module.exports = {
+  // 修改打包路径为相对路径
+  publicPath: "./",
+  configureWebpack: {
+    devServer: {
+      port: 8086, // 端口号
+      open: true,
+      // Mock数据
+      // before(app){
+
+      // }
+    },
+  },
+};
